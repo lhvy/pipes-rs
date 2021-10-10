c12216435c646871700c35e0aac01367453bb3c8
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-10-10 17:49:02 +1100

    Refactor to remove need for `Rand64`

--- crates/rng/src/lib.rs
+++ crates/rng/src/lib.rs
@@ -1,17 +1,15 @@
-use std::{fmt, ops::Range};
+use std::ops::Range;
 
 pub struct Rng {
     rand_32: oorandom::Rand32,
-    rand_64: oorandom::Rand64,
 }
 
 impl Rng {
-    pub fn new() -> Result<Self, Error> {
-        let (seed_64, seed_128) = gen_seed()?;
+    pub fn new() -> Result<Self, getrandom::Error> {
+        let seed_64 = gen_seed()?;
 
         Ok(Self {
             rand_32: oorandom::Rand32::new(seed_64),
-            rand_64: oorandom::Rand64::new(seed_128),
         })
     }
 
@@ -28,15 +26,6 @@ impl Rng {
             .rand_range(range.start as u32..range.end as u32) as u16
     }
 
-    pub fn gen_range_64(&mut self, range: Range<u64>) -> u64 {
-        self.rand_64.rand_range(range)
-    }
-
-    pub fn gen_range_size(&mut self, range: Range<usize>) -> usize {
-        self.rand_64
-            .rand_range(range.start as u64..range.end as u64) as usize
-    }
-
     pub fn gen_bool(&mut self, probability: f32) -> bool {
         assert!(probability >= 0.0);
         assert!(probability <= 1.0);
@@ -45,23 +34,9 @@ impl Rng {
     }
 }
 
-fn gen_seed() -> Result<(u64, u128), Error> {
+fn gen_seed() -> Result<u64, getrandom::Error> {
     let mut seed_64 = [0; 8];
-    getrandom::getrandom(&mut seed_64).map_err(|_| Error)?;
+    getrandom::getrandom(&mut seed_64)?;
 
-    let mut seed_128 = [0; 16];
-    getrandom::getrandom(&mut seed_128).map_err(|_| Error)?;
-
-    Ok((u64::from_le_bytes(seed_64), u128::from_le_bytes(seed_128)))
-}
-
-#[derive(Debug)]
-pub struct Error;
-
-impl std::error::Error for Error {}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "failed to get randomness from the OS")
-    }
+    Ok(u64::from_le_bytes(seed_64))
 }
