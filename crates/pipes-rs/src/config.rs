bc0aa4682be922d51b2c780b0775bec0c2e1fa5e
Luna Razzaghipour <lunarazzaghipour@gmail.com>
2023-10-16 21:29:58 +1100

    Add custom argument parsing (#130)
    * Add custom argument parsing
    
    * Remove unused `FromStr` impls
    
    * Handle `--version`/`-V`
    
    * `eprint!` -> `eprintln!`
    
    * Improve error message for bare arguments

--- crates/pipes-rs/src/config.rs
+++ crates/pipes-rs/src/config.rs
@@ -1,5 +1,4 @@
 use anyhow::Context;
-use clap::Parser;
 use etcetera::app_strategy::{AppStrategy, AppStrategyArgs, Xdg};
 use model::pipe::{ColorMode, Kind, KindSet, Palette};
 use serde::{Deserialize, Serialize};
@@ -7,66 +6,24 @@ use std::fs;
 use std::path::PathBuf;
 use std::time::Duration;
 
-#[derive(Serialize, Deserialize, Default, Parser)]
-#[command(
-    name = "pipes-rs",
-    version,
-    about = "An over-engineered rewrite of pipes.sh in Rust."
-)]
+#[derive(Serialize, Deserialize, Default)]
 pub struct Config {
-    /// what kind of terminal coloring to use
-    #[arg(short, long)]
     pub color_mode: Option<ColorMode>,
-
-    /// the color palette used assign colors to pipes
-    #[arg(long)]
     pub palette: Option<Palette>,
-
-    /// cycle hue of pipes
-    #[arg(long, value_name = "DEGREES")]
     pub rainbow: Option<u8>,
-
-    /// delay between frames in milliseconds
-    #[arg(short, long = "delay")]
     pub delay_ms: Option<u64>,
-
-    /// number of frames of animation that are displayed in a second; use 0 for unlimited
-    #[arg(short, long)]
     pub fps: Option<f32>,
-
-    /// portion of screen covered before resetting (0.0–1.0)
-    #[arg(short, long)]
     pub reset_threshold: Option<f32>,
-
-    /// kinds of pipes separated by commas, e.g. heavy,curved
-    #[arg(short, long)]
     pub kinds: Option<KindSet>,
-
-    /// whether to use bold
-    #[arg(short, long, value_name = "BOOL")]
     pub bold: Option<bool>,
-
-    /// whether pipes should retain style after hitting the edge
-    #[arg(short, long, value_name = "BOOL")]
     pub inherit_style: Option<bool>,
-
-    /// number of pipes
-    #[arg(name = "pipe-num", short, long, value_name = "NUM")]
     pub num_pipes: Option<u32>,
-
-    /// chance of a pipe turning (0.0–1.0)
-    #[arg(short, long)]
     pub turn_chance: Option<f32>,
-
-    /// Print license
-    #[arg(long)]
-    #[serde(default)]
-    pub license: bool,
 }
 
 impl Config {
     pub fn read() -> anyhow::Result<Self> {
-        let config = Self::read_from_disk_with_default()?.combine(Self::parse());
+        let config = Self::read_from_disk_with_default()?;
         config.validate()?;
 
         Ok(config)
@@ -98,7 +55,7 @@ impl Config {
         toml::from_str(&contents).context("failed to read config")
     }
 
-    fn validate(&self) -> anyhow::Result<()> {
+    pub fn validate(&self) -> anyhow::Result<()> {
         if let Some(reset_threshold) = self.reset_threshold() {
             if !(0.0..=1.0).contains(&reset_threshold) {
                 anyhow::bail!("reset threshold should be within 0 and 1")
@@ -171,22 +128,5 @@ impl Config {
 
     pub fn turn_chance(&self) -> f32 {
         self.turn_chance.unwrap_or(0.15)
-    }
-
-    fn combine(self, other: Self) -> Self {
-        Self {
-            color_mode: other.color_mode.or(self.color_mode),
-            palette: other.palette.or(self.palette),
-            rainbow: other.rainbow.or(self.rainbow),
-            delay_ms: other.delay_ms.or(self.delay_ms),
-            fps: other.fps.or(self.fps),
-            reset_threshold: other.reset_threshold.or(self.reset_threshold),
-            kinds: other.kinds.or(self.kinds),
-            bold: other.bold.or(self.bold),
-            inherit_style: other.inherit_style.or(self.inherit_style),
-            num_pipes: other.num_pipes.or(self.num_pipes),
-            turn_chance: other.turn_chance.or(self.turn_chance),
-            license: self.license || other.license,
-        }
     }
 }
